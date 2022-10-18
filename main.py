630ac4df9300ce63b5a35cb9c333e93961cb7352
Satoshi Akiyama <a-works@outlook.com>
2022-10-18 10:13:00 +0900

    Move test20

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from lcdInit import lcdInit
 from command import command
 from test import test
 from test import set_button
+from setModeChange import setmode
+from setModeChange import set_mode_change
 
 
 # I2C通信の設定　
@@ -20,14 +22,6 @@ home = 0x02
 display_On_Cursor_Off = 0x0C
 display_On_Cursor_On = 0x0f
 LCD_2ndline = 0x40+0x80
-
-# セットモードフラグ
-setmode = 0
-
-
-def set_mode_change():
-    global setmode
-    setmode = 1
 
 
 def main():
---
+++ setModeChange.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+# セットモードフラグ
+setmode = 0
+
+
+def set_mode_change():
+    global setmode
+    setmode = 1
