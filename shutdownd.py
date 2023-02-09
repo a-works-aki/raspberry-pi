188b06c25e2154e8a9ac35d1da894f39b6737882
Satoshi Akiyama <a-works@outlook.com>
2023-02-09 11:16:40 +0900

    Fix shutdownd.py-05

--- shutdownd.py
+++ shutdownd.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 from gpiozero import Button
+import os
 
-button = Button(5)
+set_button = Button(5, hold_time=2)
+shutdown_flag = 0
+
+
+def shutdown():
+    shutdown_flag = 1
+
 
 while True:
-    if button.is_pressed:
-        print("Button is pressed")
+    set_button.when_held = shutdown
+    if shutdown_flag == 1:
+        print("shutdown start")
+        os.system("sudo shutdown -h now")
     else:
         print("Button is not pressed")
