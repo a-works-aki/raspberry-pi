e6dc128599b2b1c0622ecd242ba1be5758f88cbe
Satoshi Akiyama <a-works@outlook.com>
2023-02-09 09:59:36 +0900

    Fix shutdownd.py-02

--- shutdownd.py
+++ shutdownd.py
@@ -9,7 +9,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(29, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    GPIO.wait_for_edge(27, GPIO.FALLING)
+    GPIO.wait_for_edge(29, GPIO.FALLING)
 
     while True:
         sw_status = GPIO.input(27)
