9ffd1118a2dbf3229f9cfafd52603d2bfca82ab6
Satoshi Akiyama <a-works@outlook.com>
2023-02-09 11:04:33 +0900

    Fix shutdownd.py-04

--- shutdownd.py
+++ shutdownd.py
@@ -1,31 +1,11 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/python
-# coding:utf-8
-import time
-import RPi.GPIO as GPIO
-import os
+from gpiozero import Button
 
-pinnumber = 29
-GPIO.setmode(GPIO.BCM)
-
-# GPIO29pinを入力モードとし、pull up設定とします
-GPIO.setup(pinnumber, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+button = Button(5)
 
 while True:
-    GPIO.wait_for_edge(pinnumber, GPIO.FALLING)
-    sw_counter = 0
-
-    while True:
-        sw_status = GPIO.input(pinnumber)
-        if sw_status == 0:
-            sw_counter = sw_counter + 1
-            if sw_counter >= 50:
-                print("長押し検知！")
-                os.system("sudo shutdown -h now")
-                break
-        else:
-            print("短押し検知")
-            break
-
-        time.sleep(0.01)
-
-    print(sw_counter)
+    if button.is_pressed:
+        print("Button is pressed")
+    else:
+        print("Button is not pressed")
