fb1461983f44889ee076ffd1d3c0450d553aa4d6
Satoshi Akiyama <a-works@outlook.com>
2023-02-09 16:36:46 +0900

    Fix main.py-10

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from lcdInit import lcdInit
 from command import command
 from test import test
 from test import set_button
+from gpiozero import LED
+from command import command
+from writeLcd import writeLcd
+from time import sleep
 
 # I2C通信の設定　
 data = 0x40
@@ -13,10 +17,14 @@ clear = 0x01
 home = 0x02
 display_On_Cursor_Off = 0x0C
 display_On_Cursor_On = 0x0f
+lcd_1stline = 0x80
 LCD_2ndline = 0x40+0x80
 
 # セットモードフラグ
 setmode = 0
+
+# lcdバックライト変数
+lcdLed = LED(4)
 
 
 def set_mode_change():
@@ -24,9 +32,18 @@ def set_mode_change():
     setmode = 1
 
 
+def displayStart():
+    command(lcd_1stline)
+    writeLcd("START")
+
+
 def main():
     global setmode
+    lcdLed.on()
     lcdInit()
+    displayStart()
+    sleep(1)
+    lcdLed.off()
 
     while True:
         set_button.when_held = set_mode_change
