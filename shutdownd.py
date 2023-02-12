de1ed383931b792212ea22f244e86ab0c3b7103c
Satoshi Akiyama <a-works@outlook.com>
2023-02-12 16:32:38 +0900

    Fix shutdown.py-02

--- shutdownd.py
+++ shutdownd.py
@@ -4,6 +4,8 @@ from gpiozero import Button
 import os
 from command import command
 from writeLcd import writeLcd
+from lcdInit import lcdInit
+from time import sleep
 
 shutdown_button = Button(5, hold_time=2)
 shutdown_flag = 0
@@ -23,7 +25,9 @@ def displayEnd():
 while True:
     shutdown_button.when_held = shutdown
     if shutdown_flag == 1:
+        lcdInit()
         displayEnd()
+        sleep(1)
         print("shutdown start")
         os.system("sudo shutdown -h now")
     else:
