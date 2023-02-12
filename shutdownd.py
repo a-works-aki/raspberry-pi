c9736c2ee6cae71a3e470f65cd750625c4804aa4
Satoshi Akiyama <a-works@outlook.com>
2023-02-12 17:22:30 +0900

    Fix shutdown.py-05

--- shutdownd.py
+++ shutdownd.py
@@ -6,10 +6,13 @@ from command import command
 from writeLcd import writeLcd
 from lcdInit import lcdInit
 from time import sleep
+import subprocess
+import shlex
 
 shutdown_button = Button(5, hold_time=2)
 shutdown_flag = 0
 lcd_1stline = 0x80
+clear = 0x01
 
 
 def shutdown():
@@ -22,14 +25,21 @@ def displayEnd():
     writeLcd("See You!")
 
 
-while True:
-    lcdInit()
-    displayEnd()
-    sleep(1)
-    shutdown_button.when_held = shutdown
-    if shutdown_flag == 1:
-        print("shutdown start")
-        #os.system("sudo shutdown -h now")
-    else:
-        print("Button is not pressed")
+def shutdown_work():
+    while True:
+        lcdInit()
+        displayEnd()
         sleep(1)
+        shutdown_button.when_held = shutdown
+        if shutdown_flag == 1:
+            print("shutdown start")
+            #os.system("sudo shutdown -h now")
+        else:
+            print("Button is not pressed")
+            sleep(1)
+
+
+try:
+    shutdown_work()
+finally:
+    command(clear)
