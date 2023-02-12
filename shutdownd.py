b98f627b3805caf9fb1083885600ec18bce0093b
Satoshi Akiyama <a-works@outlook.com>
2023-02-12 17:32:16 +0900

    Fix shutdown.py-06

--- shutdownd.py
+++ shutdownd.py
@@ -1,45 +1,59 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
-from gpiozero import Button
-import os
-from command import command
-from writeLcd import writeLcd
-from lcdInit import lcdInit
-from time import sleep
-import subprocess
-import shlex
+#from gpiozero import Button
+#import os
+#from command import command
+#from writeLcd import writeLcd
+#from lcdInit import lcdInit
+#from time import sleep
+#import subprocess
+#import shlex
 
-shutdown_button = Button(5, hold_time=2)
-shutdown_flag = 0
-lcd_1stline = 0x80
-clear = 0x01
+#shutdown_button = Button(5, hold_time=2)
+#shutdown_flag = 0
+#lcd_1stline = 0x80
+#clear = 0x01
+
+
+# def shutdown():
+#global shutdown_flag
+#shutdown_flag = 1
+
+
+# def displayEnd():
+# command(lcd_1stline)
+#writeLcd("See You!")
+
+
+# def shutdown_work():
+# while True:
+#shutdown_button.when_held = shutdown
+# if shutdown_flag == 1:
+# lcdInit()
+# displayEnd()
+# sleep(1)
+#print("shutdown start")
+##os.system("sudo shutdown -h now")
+# else:
+#print("Button is not pressed")
+# sleep(1)
+
+
+# try:
+# shutdown_work()
+# finally:
+# command(clear)
+
+from gpiozero import Button
+from signal import pause
+from subprocess import call
 
 
 def shutdown():
-    global shutdown_flag
-    shutdown_flag = 1
+    call("sudo shutdown -h now", shell=True)
 
 
-def displayEnd():
-    command(lcd_1stline)
-    writeLcd("See You!")
+button = Button(5)
+button.when_pressed = shutdown
 
-
-def shutdown_work():
-    while True:
-        shutdown_button.when_held = shutdown
-        if shutdown_flag == 1:
-            lcdInit()
-            displayEnd()
-            sleep(1)
-            print("shutdown start")
-            #os.system("sudo shutdown -h now")
-        else:
-            print("Button is not pressed")
-            sleep(1)
-
-
-try:
-    shutdown_work()
-finally:
-    command(clear)
+pause()
