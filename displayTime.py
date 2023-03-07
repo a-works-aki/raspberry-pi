84b15f4723ef8462341473d915f29285939b54d4
Satoshi Akiyama <a-works@outlook.com>
2023-03-07 15:05:58 +0900

    displeyTime.py LCD_1stline追加

--- displayTime.py
+++ displayTime.py
@@ -5,10 +5,12 @@ from writeLcd import writeLcd
 from command import command
 
 # I2C通信の設定　
+LCD_1stline = 0x80
 LCD_2ndline = 0x40+0x80
 
 
 def displayTime():
+    command(LCD_1stline)
     writeLcd(time.strftime("%y/%m/%d"))
     command(LCD_2ndline)
     writeLcd(time.strftime("%H:%M:%S"))
